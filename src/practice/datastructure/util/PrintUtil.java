e6259b1626b71c7c7d70ec41ed4a6c8844e067d5
lixiangyue <719977029@qq.com>
2020-02-13 16:50:19 +0800

    PrintUtil make the method static

--- src/practice/datastructure/util/PrintUtil.java
+++ src/practice/datastructure/util/PrintUtil.java
@@ -2,7 +2,7 @@ package practice.datastructure.util;
 
 public class PrintUtil {
 
-    static void printArr(int[] arr) {
+    public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(" arr[" + i + "] = " + arr[i]);
         }
