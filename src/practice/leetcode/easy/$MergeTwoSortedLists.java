a47f0f67e05c8b5323e20606a7226e7d1c91e12a
lixiangyue <719977029@qq.com>
2020-02-25 21:23:36 +0800

    the implement of Merge Two Sorted Lists

---
+++ src/practice/leetcode/easy/$MergeTwoSortedLists.java
@@ -0,0 +1,74 @@
+package practice.leetcode.easy;
+
+/**
+ * Merge two sorted linked lists and return it as a new list.
+ * The new list should be made by splicing together the nodes of the first two lists.
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->4, 1->3->4
+ * Output: 1->1->2->3->4->4
+ */
+public class $MergeTwoSortedLists {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode listNode = mergeTwoLists(l1, l2);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        while (listNode != null) {
+            if (listNode.next == null) {
+                stringBuilder.append(listNode.val);
+            }else {
+                stringBuilder.append(listNode.val + "->");
+            }
+            listNode = listNode.next;
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode sentinel = new ListNode(0);
+        ListNode cur = sentinel;
+        while (l1 != null && l2 != null) {
+            if (l1.val >= l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 == null) {
+            cur.next = l2;
+        }
+        if (l2 == null) {
+            cur.next = l1;
+        }
+        return sentinel.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
