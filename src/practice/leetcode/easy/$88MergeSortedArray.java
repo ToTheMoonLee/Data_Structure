d61b937c993ae188da28ee55723350572c6bc9b1
lixiangyue <719977029@qq.com>
2020-03-05 22:31:28 +0800

    the implement of Merge Sorted Array

---
+++ src/practice/leetcode/easy/$88MergeSortedArray.java
@@ -0,0 +1,87 @@
+package practice.leetcode.easy;
+
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ *
+ * Note:
+ *
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n)
+ * to hold additional elements from nums2.
+ * Example:
+ *
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * Output: [1,2,2,3,5,6]
+ */
+public class $88MergeSortedArray {
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        int m = 3;
+        int n = 3;
+//        merge(nums1, m, nums2, n);
+        merge2(nums1, m, nums2, n);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println(nums1[i]);
+        }
+    }
+
+    /**
+     * their solution of this problem
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+
+    /**
+     * my solution of this problem
+     *
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                temp[k++] = nums1[i++];
+            } else {
+                temp[k++] = nums2[j++];
+            }
+        }
+        while (i < m) {
+            temp[k++] = nums1[i++];
+        }
+        while (j < n) {
+            temp[k++] = nums2[j++];
+        }
+        for (int index = 0; index < m + n; index++) {
+            nums1[index] = temp[index];
+        }
+    }
+}
