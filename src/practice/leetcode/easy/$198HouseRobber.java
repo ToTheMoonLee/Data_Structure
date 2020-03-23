849549c5566d22541190175a710a8fa4c0c205b8
lixiangyue <719977029@qq.com>
2020-03-23 22:52:37 +0800

    the implement of House Robber

---
+++ src/practice/leetcode/easy/$198HouseRobber.java
@@ -0,0 +1,77 @@
+package practice.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * You are a professional robber planning to rob houses along a street.
+ * Each house has a certain amount of money stashed,
+ * the only constraint stopping you from robbing each of them is
+ * that adjacent houses have security system connected and
+ * it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers representing the amount of money of each house,
+ * determine the maximum amount of money you can rob tonight without alerting the police.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ *              Total amount you can rob = 1 + 3 = 4.
+ * Example 2:
+ *
+ * Input: [2,7,9,3,1]
+ * Output: 12
+ * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+ *              Total amount you can rob = 2 + 9 + 1 = 12.
+ */
+public class $198HouseRobber {
+
+    static int[] memo;
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 9, 3, 1};
+        System.out.println(rob(nums));
+        System.out.println(rob2(nums));
+    }
+
+    /**
+     * their second solution of this problem
+     * @param nums
+     * @return
+     */
+    static int rob2(int[] nums) {
+        int pre1 = 0;
+        int pre2 = 0;
+        int result = 0;
+        for (int i=0;i<nums.length;i++) {
+            result = Math.max(pre1 + nums[i], pre2);
+            pre1 = pre2;
+            pre2 = result;
+        }
+        return result;
+    }
+
+    /**
+     * their first solution of this problem
+     * @param nums
+     * @return
+     */
+    static int rob(int[] nums) {
+        memo = new int[nums.length + 1];
+        Arrays.fill(memo, -1);
+        return rob(nums, nums.length - 1);
+    }
+
+    static int rob(int[] nums, int i) {
+        if (i<0) {
+            return 0;
+        }
+        if (memo[i]!=-1) {
+            return memo[i];
+        }
+        int result = Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+        memo[i] = result;
+        return result;
+    }
+}
