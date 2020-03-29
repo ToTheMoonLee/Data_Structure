0eedf0b3316b9b7c4fc2f37e1cee3c602e98edf4
lixiangyue <719977029@qq.com>
2020-03-29 17:05:19 +0800

    their solution of this problem

--- src/practice/leetcode/easy/$219ContainsDuplicate2.java
+++ src/practice/leetcode/easy/$219ContainsDuplicate2.java
@@ -1,6 +1,8 @@
 package practice.leetcode.easy;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Given an array of integers and an integer k,
@@ -25,6 +27,22 @@ public class $219ContainsDuplicate2 {
     public static void main(String[] args) {
         System.out.println(containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
         System.out.println(containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 1));
+    }
+
+    /**
+     * their solution of this problem
+     * using a sliding window int the iterate
+     * @param nums
+     * @param k
+     * @return
+     */
+    static boolean containsNearbyDuplicate2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
     }
 
     /**
