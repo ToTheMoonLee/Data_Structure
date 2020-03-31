5dc82add712ad410ab8812501088969cc373efee
lixiangyue <719977029@qq.com>
2020-03-31 19:44:16 +0800

    their solution of this problem

---
+++ src/practice/leetcode/easy/$290WordPattern.java
@@ -0,0 +1,31 @@
+package practice.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class $290WordPattern {
+
+    public static void main(String[] args) {
+        String s1 = "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccdd";
+        String s2 = "s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s t t";
+
+        System.out.println(wordPattern(s1, s2));
+    }
+
+    /**
+     * their amazing solution of this problem
+     * @param pattern
+     * @param str
+     * @return
+     */
+    static boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
+        return true;
+    }
+}
