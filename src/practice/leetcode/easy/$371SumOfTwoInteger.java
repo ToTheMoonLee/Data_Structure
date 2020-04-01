e049e799e20c7f4838077e97815bddf9cbac5b97
lixiangyue <719977029@qq.com>
2020-04-01 20:34:32 +0800

    read the discussion here (https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary%3A-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently)

---
+++ src/practice/leetcode/easy/$371SumOfTwoInteger.java
@@ -0,0 +1,25 @@
+package practice.leetcode.easy;
+
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ *
+ * Example 1:
+ *
+ * Input: a = 1, b = 2
+ * Output: 3
+ * Example 2:
+ *
+ * Input: a = -2, b = 3
+ * Output: 1
+ */
+public class $371SumOfTwoInteger {
+
+    public static void main(String[] args) {
+        /**
+         * I should read this discussion
+         * <a href="https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary%3A-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently"/>
+         */
+    }
+
+
+}
