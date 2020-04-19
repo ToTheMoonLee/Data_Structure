a30a6134bb64d0e29fffb0adf4be4aa365e526d6
lixiangyue <719977029@qq.com>
2020-04-19 20:18:45 +0800

    the implement of Min Cost Climbing Stairs

---
+++ src/practice/leetcode/easy/$746MinCostClimbingStairs.java
@@ -0,0 +1,50 @@
+package practice.leetcode.easy;
+
+/**
+ * 746. Min Cost Climbing Stairs
+ * Easy
+ *
+ * 1693
+ *
+ * 387
+ *
+ * Add to List
+ *
+ * Share
+ * On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+ *
+ * Once you pay the cost, you can either climb one or two steps.
+ * You need to find minimum cost to reach the top of the floor,
+ * and you can either start from the step with index 0, or the step with index 1.
+ *
+ * Example 1:
+ * Input: cost = [10, 15, 20]
+ * Output: 15
+ * Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+ * Example 2:
+ * Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+ * Output: 6
+ * Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+ * Note:
+ * cost will have a length in the range [2, 1000].
+ * Every cost[i] will be an integer in the range [0, 999].
+ */
+public class $746MinCostClimbingStairs {
+
+    public static void main(String[] args) {
+        int[] cost = new int[]{1,100,1,1,1,100,1,1,100,1};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+
+    /**
+     * their dynamic programming solution of this problem
+     * @param cost
+     * @return
+     */
+    static int minCostClimbingStairs(int[] cost) {
+        for (int i=2;i<cost.length;i++) {
+            cost[i] += Math.min(cost[i-1],cost[i-2]);
+        }
+        return Math.min(cost[cost.length-1],cost[cost.length-2]);
+    }
+}
