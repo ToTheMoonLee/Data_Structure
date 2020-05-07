2d38be3a35d4b5899ae6b2f56eafe1330ca9190a
lixiangyue <719977029@qq.com>
2020-05-07 22:01:07 +0800

    the implement of Cousin in Binary Tree

---
+++ src/practice/leetcode/easy/$993CousinsInBinaryTree.java
@@ -0,0 +1,86 @@
+package practice.leetcode.easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * In a binary tree, the root node is at depth 0, and children of each depth k node are at depth k+1.
+ *
+ * Two nodes of a binary tree are cousins if they have the same depth, but have different parents.
+ *
+ * We are given the root of a binary tree with unique values,
+ * and the values x and y of two different nodes in the tree.
+ *
+ * Return true if and only if the nodes corresponding to the values x and y are cousins.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [1,2,3,4], x = 4, y = 3
+ * Output: false
+ * Example 2:
+ *
+ *
+ * Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
+ * Output: true
+ * Example 3:
+ *
+ *
+ *
+ * Input: root = [1,2,3,null,4], x = 2, y = 3
+ * Output: false
+ *
+ *
+ * Note:
+ *
+ * The number of nodes in the tree will be between 2 and 100.
+ * Each node has a unique integer value from 1 to 100.
+ */
+public class $993CousinsInBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+        System.out.println(isCousins(root, 4, 5));
+    }
+
+    /**
+     * their solution of this problem
+     * @param root
+     * @param x
+     * @param y
+     * @return
+     */
+    static boolean isCousins(TreeNode root, int x, int y) {
+        if (root == null) return false;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            boolean isX = false;
+            boolean isY = false;
+            while (size-- > 0) {
+                TreeNode temp = q.poll();
+                if (temp.val == x) isX = true;
+                if (temp.val == y) isY = true;
+                if (temp.left!=null && temp.right!=null) {
+                    if (temp.left.val == x && temp.right.val == y) return false;
+                    if (temp.left.val == y && temp.right.val == x) return false;
+                }
+                if (temp.left!=null) {
+                    q.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    q.offer(temp.right);
+                }
+            }
+            if (isX && isY) return true;
+        }
+        return false;
+    }
+}
