52b461eac9f96f5ff514927ed65545d56364b3f2
lixiangyue <719977029@qq.com>
2020-07-07 13:58:47 +0800

    the implement of Number of Steps to Reduce a Number to Zero

---
+++ src/practice/leetcode/easy/$1342NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,80 @@
+package practice.leetcode.easy;
+
+/**
+ * Given a non-negative integer num, return the number of steps to reduce it to zero.
+ * If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: num = 14
+ * Output: 6
+ * Explanation:
+ * Step 1) 14 is even; divide by 2 and obtain 7.
+ * Step 2) 7 is odd; subtract 1 and obtain 6.
+ * Step 3) 6 is even; divide by 2 and obtain 3.
+ * Step 4) 3 is odd; subtract 1 and obtain 2.
+ * Step 5) 2 is even; divide by 2 and obtain 1.
+ * Step 6) 1 is odd; subtract 1 and obtain 0.
+ * Example 2:
+ * <p>
+ * Input: num = 8
+ * Output: 4
+ * Explanation:
+ * Step 1) 8 is even; divide by 2 and obtain 4.
+ * Step 2) 4 is even; divide by 2 and obtain 2.
+ * Step 3) 2 is even; divide by 2 and obtain 1.
+ * Step 4) 1 is odd; subtract 1 and obtain 0.
+ * Example 3:
+ * <p>
+ * Input: num = 123
+ * Output: 12
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= num <= 10^6
+ */
+public class $1342NumberOfStepsToReduceANumberToZero {
+
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(123));
+        System.out.println(numberOfSteps2(123));
+    }
+
+    /**
+     * their binary solution of this problem
+     *
+     * @param num
+     * @return
+     */
+    static int numberOfSteps2(int num) {
+        if (num == 0) return 0;
+        int ans = 0;
+        while (num != 0) {
+            ans += (num & 1) == 0 ? 1 : 2;
+            num >>= 1;
+        }
+        return ans - 1;
+    }
+
+    /**
+     * my first solution of this problem
+     *
+     * @param num
+     * @return
+     */
+    static int numberOfSteps(int num) {
+        int ans = 0;
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}
